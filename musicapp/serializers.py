236d2f2860489c6b05ca3ea4dde75737ebe19368
Zhang <Gordon.Zhang@moneris.com>
2024-09-30 13:05:08 -0400

    reorg

--- musicapp/serializers.py
+++ musicapp/serializers.py
@@ -2,17 +2,16 @@ from rest_framework import serializers
 from .models import Recommendation, Track
 from django.contrib.auth.models import User
 
+class TrackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Track
+        fields = ['track_id', 'track_name', 'track_uri', 'album_name', 'album_art', 'album_release_date', 'artist_id', 'artist_name', 'artist_uri']
+
 class RecommendationSerializerPOST(serializers.ModelSerializer):
     class Meta:
         model = Recommendation
         fields = ['user', 'title', 'timestamp']
         extra_kwargs = {"user": {"read_only": True}}
-
-
-class TrackSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Track
-        fields = ['track_id', 'track_name', 'track_uri', 'album_name', 'album_art', 'album_release_date', 'artist_id', 'artist_name', 'artist_uri']
 
 class RecommendationSerializer(serializers.ModelSerializer):
     tracks = TrackSerializer(many=True)
